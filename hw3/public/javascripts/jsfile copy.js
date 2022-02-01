defe9744c47cb26c9ac162ec9ba3209f343a9531
tashchuk23 <tashchuk23@up.edu>
2022-01-31 21:29:21 -0800

    first commit, spec 5 completed

---
+++ hw3/public/javascripts/jsfile copy.js
@@ -0,0 +1,36 @@
+
+
+
+<script>
+  $(document).ready(function(){
+      $("#orderconf").hide();
+      $("#orderb").click(function(){
+	    var mess = $("#texta").val();
+	  if (mess.toLowerCase().indexOf("vegan") >= 0){
+	      alert("Cheesecake contains dairy");
+	  } else {
+	      $(".formc").hide();
+	      $("#orderconf").delay(300).slideDown(400);
+	      var quantity =  $("#qops").val();
+	      var whichtop = $('input[name="topping"]:checked').val();
+	      document.getElementById("orderconf").innerHTML +=
+		   quantity + " pieces of " + whichtop + " with the note: " + mess;
+	   
+	  } 
+      });
+    });
+</script>
+    <script>
+  $(document).ready(function(){
+    $("a.month").click(function(){
+	var monthval = $(this).attr("value");
+	$("#dropbut").text(monthval);	
+  });
+  });
+
+
+</script>
+
+
+
+  
